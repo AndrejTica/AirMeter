80901397b21e9f90fbb2ea68e5a68b846b022098
Andrej <andrej@pop-os.localdomain>
2020-12-15 21:43:51 +0100

    parse?

--- website/index.js
+++ website/index.js
@@ -1,89 +1,77 @@
 //document.addEventListener('DOMContentLoaded', () =>{    //callback function
   
-    var d = new Date();
-    var t = d.getDate(); //get tag
-    var m = d.getMonth() + 1; //get monat, plus one cuz january is 0
-    var j = d.getFullYear();
-    var tagString = t.toString();   //we need to convert the numbers to strings
-    var monatString = m.toString();
-    var jahrString = j.toString();
+var d = new Date();
+var t = d.getDate(); //get tag
+var m = d.getMonth() + 1; //get monat, plus one cuz january is 0
+var j = d.getFullYear();
+var tagString = t.toString();   //we need to convert the numbers to strings
+var monatString = m.toString();
+var jahrString = j.toString();
 
 
-    var options = {
-        chart:{
+var options = {
+    chart:{
 
-            zoomType: 'xy'
+        zoomType: 'xy'
 
-        },
+    },
 
-        // tooltip:{
-        //     formatter(){
-        //         return `Zeit - ${this.x}. ppm - ${this.y}`;
+    exporting:{
+        enabled: true
+    },
 
-        //     }
+    credits:{                   //credit aus
+        enabled: false
+    },
 
-        // },
+    title: {
+        text: 'Airmeter: ' + tagString +'.'+ monatString + '.' + jahrString //just d for full time
+    },
 
-        exporting:{
-            enabled: true
-        },
+    xAxis:{
+     
+        title:{
+            text: 'Zeit'
+        }
+    },
 
-        credits:{                   //credit aus
-            enabled: false
-        },
+    yAxis:{
+      
 
-        title: {
-            text: 'Airmeter: ' + tagString +'.'+ monatString + '.' + jahrString //just d for full time
-        },
+        title:{
+            text: 'CO2 in ppm'
+        }
 
-        xAxis:{
-            type: 'Uhrszeit', //funktioniert nur wenn ich monat und tag auch hab
-            title:{
-                text: 'Zeit'
-            }
-        },
+    } 
+};
 
-        yAxis:{
-          
-
-            title:{
-                text: 'CO2 in ppm'
-            }
-
-        } 
+   
+ $.get('test.csv', csv => {
+    options.data = {
+        csv
     };
-       
-       
-     $.get('test.csv', csv => {
-        options.data = {
-            csv
-        };
-        Highcharts.chart('container', options); //highchart global variable from library 
-     });
+    Highcharts.chart('container', options); //highchart global variable from library 
+ });
 
 
-     function parseCSVData(csvFile){
-        //empty array for storing our data
-        var data = [];
-        //split into lines
-        var lines = csvFile.split("\n");
-        //go through each line
-        $.each(lines, function (lineNumber, line){
-            if (lineNumber != 0){
-                var fields = line.split(",");
-                if (fields.lenght == 7){
-                    var timestamp = Date.parse(fields[0]);
-                    var value = parseFloat(fields[6]);
-                    data.push([timestamp, value]);
-                }
+ function parseCSVData(csv){
+    //empty array for storing our data
+    var data = [];
+    //split into lines
+    var lines = csv.split("\n");
+    //go through each line
+    $.each(lines, function (lineNumber, line){
+        if (lineNumber != 0){
+            var fields = line.split(",");
+            if (fields.lenght == 7){
+                var timestamp = Date.parse(fields[0]);
+                data.push([timestamp]);
             }
+        }
 
-        });
-     }
-
-  
-
-
+    });
+    return data.reverse();
+ }
 
 
 
@@ -98,12 +86,16 @@
 
 
 
-    // options.data = {
-    //     csvURL: '/home/andrej/Documents/LastSchoolYear/Airmeter/website/test.csv',
-    //     enablePolling: true,
-    //     dataRefreshRate: 2
-    // }
-    // Highcharts.chart('container', options);
+
+
+
+
+// options.data = {
+//     csvURL: '/home/andrej/Documents/LastSchoolYear/Airmeter/website/test.csv',
+//     enablePolling: true,
+//     dataRefreshRate: 2
+// }
+// Highcharts.chart('container', options);
 //});
 
 
@@ -112,5 +104,4 @@
 
 
 /*
-
 */
