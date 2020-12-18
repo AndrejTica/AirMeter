37b2370d49745db9f7bf73ca85e8d4a4b92fa4c9
Andrej Tica <andrejtica@gmail.com>
2020-12-18 14:30:50 +0100

    Date title complete

--- website/index.js
+++ website/index.js
@@ -1,107 +1,75 @@
-//document.addEventListener('DOMContentLoaded', () =>{    //callback function
+$.ajax({
+  type: "GET",
+  url: "test.csv",
+  success: function (data) {
+    setTitle(data)
+  }
+});
+
+
+function setTitle(raw_data){
+
+  let newTitle;
+ 
   
-var d = new Date();
-var t = d.getDate(); //get tag
-var m = d.getMonth() + 1; //get monat, plus one cuz january is 0
-var j = d.getFullYear();
-var tagString = t.toString();   //we need to convert the numbers to strings
-var monatString = m.toString();
-var jahrString = j.toString();
-
-
-var options = {
-    chart:{
-
-        zoomType: 'xy'
-
+  let chart = Highcharts.chart('container', {
+    
+    chart: {
+      zoomType: 'xy',
+      events: {
+        load: function() {
+          this.update({
+            title: {
+              text: 'Airmeter: '+ newTitle
+            }
+          })
+        }
+        
+      }
+    
     },
+    
+    xAxis:{
+     
+      title:{
+          text: 'Zeit'
+      }
+  },
 
-    exporting:{
-        enabled: true
+  yAxis:{
+      
+
+    title:{
+        text: 'CO2 in ppm'
+    }
+
+} ,
+  
+exporting:{
+  enabled: true
+},
+
+
+    title: {
+      text: null
     },
 
     credits:{                   //credit aus
-        enabled: false
-    },
+      enabled: false
+  },
 
-    title: {
-        text: 'Airmeter: ' + tagString +'.'+ monatString + '.' + jahrString //just d for full time
-    },
-
-    xAxis:{
-     
-        title:{
-            text: 'Zeit'
-        }
-    },
-
-    yAxis:{
+    data: {
+      csv: raw_data,
+      parsed(e) {
+        newTitle=e[0][1]
+        e.shift()
+        
+      }
       
+    }
+  
+  
+  });
 
-        title:{
-            text: 'CO2 in ppm'
-        }
+}
 
-    } 
-};
-
-   
- $.get('test.csv', csv => {
-    options.data = {
-        csv
-    };
-    Highcharts.chart('container', options); //highchart global variable from library 
- });
-
-
- function parseCSVData(csv){
-    //empty array for storing our data
-    var data = [];
-    //split into lines
-    var lines = csv.split("\n");
-    //go through each line
-    $.each(lines, function (lineNumber, line){
-        if (lineNumber != 0){
-            var fields = line.split(",");
-            if (fields.lenght == 7){
-                var timestamp = Date.parse(fields[0]);
-                data.push([timestamp]);
-            }
-        }
-
-    });
-    return data.reverse();
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// options.data = {
-//     csvURL: '/home/andrej/Documents/LastSchoolYear/Airmeter/website/test.csv',
-//     enablePolling: true,
-//     dataRefreshRate: 2
-// }
-// Highcharts.chart('container', options);
-//});
-
-
-
-
-
-
-/*
-*/
