18bbe26493677e26afd2aba898441daf353ff0e7
Andrej Tica <andrejtica@gmail.com>
2021-02-11 20:49:56 +0100

    USB_SSH and cleanup

--- website/chromium_noCors.sh
+++ website/chromium_noCors.sh
@@ -4,6 +4,6 @@
 cd 
 mkdir -p cors
 name=$(whoami)
-chromium-browser --user-data-dir=/home/name/cors --disable-web-security --disable-site-isolation-trials /home/andre/Airmeter/website/index.html
+chromium-browser --user-data-dir=/home/name/cors --disable-web-security --disable-site-isolation-trials /home/name/Airmeter/website/index.html
 
 exit
