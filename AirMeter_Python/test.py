75d2bc7cb13bc9726b841a2f875bac95ee3441ac
Andrej Tica <andrejtica@gmail.com>
2021-03-30 01:53:31 +0200

    only website

--- AirMeter_Python/test.py
+++
@@ -1,6 +0,0 @@
-from PAS_CO2_LIB import *
-
-_,_,status, meas_status, result = Measure(1019)
-print(status)
-print(meas_status)
-print(result)
--- website (copy)/chromium_noCors.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-cd 
-mkdir -p cors
-name=$(whoami)
-chromium-browser --user-data-dir=/home/name/cors --disable-web-security --disable-site-isolation-trials /home/andre/Airmeter/website/index.html
-
-exit
